2211c2fa205ed801009117f24d400ea715de6645
unknown <suyashgaikwad31@gmail.com>
2021-03-11 11:33:42 +0530

    added EmpWageBuilderuc1.java

--- EmpWageBuilderuc1.java
+++ EmpWageBuilderuc1.java
@@ -1,16 +1,20 @@
-public class EmpWageBuilderFor {
+public class EmpWageBuilderWhile {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
     public static final int NUM_OF_WORKING_DAYS = 2;
+    public static final int MAX_HRS_IN_MONTH = 10;
+
 
         public static void main(String [ ] args) {
                 //Variables
                 int empHrs = 0;
-                int empWage = 0;
-                int totalEmpWage = 0;
+                int totalEmpHrs = 0;
+                int totalWorkingDays = 0;
                 // Computation
-                for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+                while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
+                        totalWorkingDays < NUM_OF_WORKING_DAYS) {
+                   totalWorkingDays++;
                 int empcheck = (int) Math.floor(Math.random() * 10) % 3;
                 switch (empcheck) {
                     case IS_PART_TIME:
@@ -22,13 +26,17 @@ public class EmpWageBuilderFor {
                     default:
                         empHrs = 0;
                 }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
-                System.out.println("Emp Wage: " + empWage);
+                totalEmpHrs += empHrs;
+                System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +empHrs);
           }
+          int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
           System.out.println("Total Emp Wage: " + totalEmpWage);
        }
 }
+
+
+
+
 
 
 
