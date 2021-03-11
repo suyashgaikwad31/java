dea0a95d0a889b5a3ecfbd8e8f3f89aec3ea7325
unknown <suyashgaikwad31@gmail.com>
2021-03-11 11:26:14 +0530

    added EmpWageBuilderuc1.java

--- EmpWageBuilderuc1.java
+++ EmpWageBuilderuc1.java
@@ -1,13 +1,16 @@
-public class EmpWageBuilderCase {
+public class EmpWageBuilderFor {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 2;
 
         public static void main(String [ ] args) {
                 //Variables
                 int empHrs = 0;
                 int empWage = 0;
+                int totalEmpWage = 0;
                 // Computation
+                for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
                 int empcheck = (int) Math.floor(Math.random() * 10) % 3;
                 switch (empcheck) {
                     case IS_PART_TIME:
@@ -20,9 +23,14 @@ public class EmpWageBuilderCase {
                         empHrs = 0;
                 }
                 empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
                 System.out.println("Emp Wage: " + empWage);
-        }
+          }
+          System.out.println("Total Emp Wage: " + totalEmpWage);
+       }
 }
+
+
 
 
 
