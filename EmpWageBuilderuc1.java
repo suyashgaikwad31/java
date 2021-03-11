fb0e0a7523c4594cf83f49b3fc75f873835d4106
unknown <suyashgaikwad31@gmail.com>
2021-03-11 11:04:24 +0530

    added EmpWageBuilderuc1.java

--- EmpWageBuilderuc1.java
+++ EmpWageBuilderuc1.java
@@ -1,14 +1,22 @@
-public class EmpWageBuilderUC1 {
+public class EmpWageBuilderUC2 {
 
         public static void main(String [ ] args) {
                 // Constants
                 int IS_FULL_TIME = 1;
+                int EMP_RATE_PER_HOUR = 20;
+                //Variables
+                int empHrs = 0;
+                int empWage = 0;
                         // Computation
                 double empcheck = Math.floor(Math.random() * 10) % 2;
                 if (empcheck == IS_FULL_TIME)
-                        System.out.println("Employee is Present");
+                    empHrs = 8;
                 else
-                        System.out.println("Employee is Absent");
+                    empHrs = 0;
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                System.out.println("Emp Wage: " + empWage);
         }
 }
+
+
 
